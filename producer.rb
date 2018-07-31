8606b9d82b9a8e48d817aa62c451faff07aa4986
Mona Dadoun <mona@84codes.com>
2018-07-31 15:25:11 +0200

    Add cloudKarafka producer

---
+++ producer.rb
@@ -0,0 +1,25 @@
+require 'bundler/setup'
+require 'rdkafka'
+require 'dotenv/load'
+
+config = {
+          :"bootstrap.servers" => ENV['CLOUDKARAFKA_BROKERS'],
+          :"group.id"          => "cloudkarafka-example",
+          :"sasl.username"     => ENV['CLOUDKARAFKA_USERNAME'],
+          :"sasl.password"     => ENV['CLOUDKARAFKA_PASSWORD'],
+          :"security.protocol" => "SASL_SSL",
+          :"sasl.mechanisms"   => "SCRAM-SHA-256"
+}
+topic = "#{ENV['CLOUDKARAFKA_TOPIC_PREFIX']}test"
+
+rdkafka = Rdkafka::Config.new(config)
+producer = rdkafka.producer
+
+100.times do |i|
+  puts "Producing message #{i}"
+  producer.produce(
+      topic:   topic,
+      payload: "Payload #{i}",
+      key:     "Key #{i}"
+  ).wait
+end
